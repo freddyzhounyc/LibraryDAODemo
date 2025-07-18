45b8e014d60f3590318679dfc81d8cfadfdcfda6
Freddy Zhou <freddyzhou06@gmail.com>
2025-07-17 21:57:30 -0400

    Add back in original files, but removed schema.sql as it is not used and will cause errors.

---
+++ src/main/java/com/freddyzhounyc/databaseDemo/dao/BookDao.java
@@ -0,0 +1,14 @@
+//package com.freddyzhounyc.databaseDemo.dao;
+//
+//import com.freddyzhounyc.databaseDemo.domain.Book;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//public interface BookDao {
+//    void create(Book book);
+//    Optional<Book> findOne(String isbn);
+//    List<Book> find();
+//    void update(String isbn, Book book);
+//    void delete(String isbn);
+//}
